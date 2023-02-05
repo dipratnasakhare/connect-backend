086465e1a3419a7286fd1bd083651afb7890f470
dipratnasakhare <dipratnasakhare11@email.com>
2023-02-05 20:31:57 +0530

    adding server for flowrry shop

---
+++ config/db.js
@@ -0,0 +1,9 @@
+
+const mongoose = require("mongoose")
+mongoose.set('strictQuery', false)
+require('dotenv').config();
+
+
+connect = mongoose.connect(process.env.MONGOOSE_URL)
+
+module.exports = { connect }
--- index.js
+++ index.js
@@ -1,7 +1,55 @@
-const express = require('express')
+
+const express = require("express")
+const { connect } = require("./config/db")
+const cors = require("cors");
+require('dotenv').config();
+
+// Routes here 
+const { UserAuthRoutes } = require("./Routes/UserAuth.Routes")
+const { ValentineDay } = require("./Routes/Valentine-Day.Routes")
+const { GiftBasketsFood } = require("./Routes/Gift-Baskets-Food.Routes");
+const { LavenderGifts } = require("./Routes/lavender-gifts.Routes");
+const { BirthdayFlowers } = require("./Routes/Birthday-Flowers.Routes");
+
+
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+app.use(cors())
+
+
+app.use(express.json());
+app.use("/valentine_Day",ValentineDay)
+app.use("/Gift_Baskets_Food",GiftBasketsFood)
+app.use("/Lavender_Gifts",LavenderGifts)
+app.use("/Birthday_Flowers",BirthdayFlowers)
+
+// app.use("/Lilies",Lilies)
+// app.use("/Gardenia_Plants",GardeniaPlants)
+// app.use("/Alstroemeria",Alstroemeria)
+// app.use("/Exotic",Exotic)
+// app.use("/Sweet_Flower",SweetFlower)
+// app.use("/Blue",Blue)
+// app.use("/Green",Green)
+// app.use("/Red",Red)
+// app.use("/Purple",Purple)
+// app.use("/Bright",Bright)
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("Welcome To Backend")
 })
-app.listen(process.env.PORT || 3000)
+app.use(express.json());
+app.use("/user",UserAuthRoutes)
+
+
+app.listen(process.env.MONGOOSE_PORT_SERVER, async ()=> {
+  try{
+      await connect
+      console.log("connected to db")
+      console.log({msg:"Your server is running at port"},  process.env.MONGOOSE_PORT_SERVER)
+  }catch(err){
+      console.log("connection failed to db")
+      console.log(err)
+  }
+})
